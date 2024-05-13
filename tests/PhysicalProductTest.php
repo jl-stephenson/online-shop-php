bc31cb7f8bf73964ec1a99e7d6e7c2e2c24e37a8
John <johnlouisstephenson@gmail.com>
2024-05-13 15:53:11 +0100

    Set up test of PhysicalProduct

---
+++ tests/PhysicalProductTest.php
@@ -0,0 +1,18 @@
+<?php
+
+require_once '../src/PhysicalProduct.php';
+
+use PHPUnit\Framework\TestCase;
+
+class PhysicalProductTest extends TestCase
+{
+    public function testPhysicalProductConstruct()
+    {
+        $physicalProduct = new PhysicalProduct('Book', 5.99, 2, 'Not bad');
+        $this->assertInstanceOf(PhysicalProduct::class, $physicalProduct);
+        $this->assertSame('Book', $physicalProduct->name);
+        $this->assertSame(5.99, $physicalProduct->price);
+        $this->assertSame(2.0, $physicalProduct->weight);
+        $this->assertSame('Not bad', $physicalProduct->description);
+    }
+}
