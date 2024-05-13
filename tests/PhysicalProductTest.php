23ac9b95cb456343e6c9c80e69520f00eb7f81e6
John <johnlouisstephenson@gmail.com>
2024-05-13 16:03:26 +0100

    Updated Physical Product Tests, completed constructor tests

--- tests/PhysicalProductTest.php
+++ tests/PhysicalProductTest.php
@@ -21,4 +21,22 @@ class PhysicalProductTest extends TestCase
         $this->expectException(TypeError::class);
         new PhysicalProduct(([]), 5.99, 2, 'Not bad');
     }
+
+    public function testPhysicalProductConstruct_malformedPrice()
+    {
+        $this->expectException(TypeError::class);
+        new PhysicalProduct('Bob', 'hello', 2, 'Not bad');
+    }
+
+    public function testPhysicalProductConstruct_malformedWeight()
+    {
+        $this->expectException(TypeError::class);
+        new PhysicalProduct('Bob', 5.99, 'Big', 'Not bad');
+    }
+
+    public function testPhysicalProductConstruct_malformedDescription()
+    {
+        $this->expectException(TypeError::class);
+        new PhysicalProduct('Bob', 5.99, 2, ([]));
+    }
 }
